5015708158cefab396b5662110e6e9c19bf6ca39
phizercost <phizer.development@gmail.com>
2019-04-05 12:17:39 +0200

    Proof of work

--- block.test.js
+++ block.test.js
@@ -8,13 +8,17 @@ describe('Block', () => {
     const lastHash = 'foo-hash';
     const hash = 'hash';
     const data = ['blockhain', 'data'];
-    const block = new Block({timestamp, lastHash,hash, data});
+    const nonce = 1;
+    const difficulty = 1
+    const block = new Block({timestamp, lastHash,hash, data, nonce, difficulty});
 
     it('has a timestamp, lastHash, hash and data properties', () => {
         expect(block.timestamp).toEqual(timestamp);
         expect(block.lastHash).toEqual(lastHash);
         expect(block.hash).toEqual(hash);
         expect(block.data).toEqual(data);
+        expect(block.nonce).toEqual(nonce);
+        expect(block.difficulty).toEqual(difficulty);
     });
 });
 
@@ -50,6 +54,11 @@ describe('mineBlock', () =>{
     });
 
     it('creates a SHA-256 `hash` based on the proper inputs', () => {
-        expect(minedBlock.hash).toEqual(cryptoHash(minedBlock.timestamp, lastBlock.hash, data));
+        expect(minedBlock.hash).toEqual(cryptoHash(minedBlock.timestamp, minedBlock.nonce, minedBlock.difficulty, lastBlock.hash, data));
+    });
+
+    it('sets a `hash` that matches the difficulty criteria', () => {
+        expect(minedBlock.hash.substring(0, minedBlock.difficulty))
+            .toEqual('0'.repeat(minedBlock.difficulty));
     });
 });
--- blockchain.js
+++ blockchain.js
@@ -35,10 +35,10 @@ class Blockchain  {
         }
 
         for (let i=1; i<chain.length; i++){
-            const {timestamp, lastHash, hash, data} = chain[i];
+            const {timestamp, lastHash, hash, nonce, difficulty, data} = chain[i];
             const actualLashHash = chain[i-1].hash;
             if (lastHash !== actualLashHash) return false;
-            const validatedHash = cryptoHash(timestamp, lastHash, data);
+            const validatedHash = cryptoHash(timestamp, lastHash, data, nonce, difficulty);
             if (hash !== validatedHash) return false;
         }
         return true;
