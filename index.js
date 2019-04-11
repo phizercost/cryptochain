330100a5935ffdac847cad4982200ee62b0adf9c
phizercost <phizer.development@gmail.com>
2019-04-11 15:25:09 +0200

    Broadcast chain and starting peers

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const Blockchain = require('./blockchain');
-
+const PubSub = require('./pubsub');
 
 const app = express();
 const blockchain = new Blockchain();
+const pubsub = new PubSub({ blockchain });
+
+
+setTimeout(() => pubsub.broadcastChain(), 1000);
 
 app.use(bodyParser.json());
 
@@ -17,8 +21,18 @@ app.post('/api/mine', (req, res) => {
 
     blockchain.addBlock({ data});
 
+    pubsub.broadcastChain();
+
     res.redirect('/api/blocks');
 });
 
-const PORT = 3000; 
+
+const DEFAULT_PORT = 3000;
+let PEER_PORT;
+
+if(process.env.GENERATE_PEER_PORT === 'true') {
+    PEER_PORT = DEFAULT_PORT + 1-1000; Math.ceil(Math.random() * 1000);
+}
+
+const PORT = PEER_PORT || DEFAULT_PORT; 
 app.listen(PORT, () => console.log(`listening at localhost:${PORT}`));
