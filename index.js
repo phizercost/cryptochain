6e7bfa07986c78a06bea51b4e52c9b5978afa2a8
phizercost <phizer.development@gmail.com>
2019-04-11 15:37:26 +0200

    Synchnozing chains on connect

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const request = require('request');
 const Blockchain = require('./blockchain');
 const PubSub = require('./pubsub');
 
 const app = express();
 const blockchain = new Blockchain();
 const pubsub = new PubSub({ blockchain });
+
+const DEFAULT_PORT = 3000;
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
 
 
 setTimeout(() => pubsub.broadcastChain(), 1000);
@@ -26,8 +30,16 @@ app.post('/api/mine', (req, res) => {
     res.redirect('/api/blocks');
 });
 
+const syncChains = () => {
+    request({ url: `${ROOT_NODE_ADDRESS}/api/blocks`}, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+            const rootChain = JSON.parse(body);
+            console.log('replace chain on a sync with ', rootChain);
+            blockchain.replaceChain(rootChain);
+        }
+    });
+};
 
-const DEFAULT_PORT = 3000;
 let PEER_PORT;
 
 if(process.env.GENERATE_PEER_PORT === 'true') {
@@ -35,4 +47,7 @@ if(process.env.GENERATE_PEER_PORT === 'true') {
 }
 
 const PORT = PEER_PORT || DEFAULT_PORT; 
-app.listen(PORT, () => console.log(`listening at localhost:${PORT}`));
+app.listen(PORT, () => {
+    console.log(`listening at localhost:${PORT}`);
+    syncChains();
+});
