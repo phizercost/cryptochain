e454f464b7397a1186861f145d165e6b6ffcdbd4
phizercost <phizer.development@gmail.com>
2019-04-18 19:02:46 +0200

    Transaction validation

--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -1,7 +1,7 @@
 const Transaction = require ('./transaction');
 const Wallet = require('./index');
 const { verifySignature } = require('../util');
-describe('Transction', () => {
+describe('Transaction', () => {
     let transaction, senderWallet, recipient, amount;
 
     beforeEach(() => {
@@ -55,6 +55,44 @@ describe('Transction', () => {
                     signature: transaction.input.signature
                 })
             ).toBe(true);
+        });
+    });
+
+
+   
+    describe('validTransaction()', () => {
+        let errorMock;
+
+        beforeEach(() => {
+            errorMock = jest.fn();
+            global.console.error = errorMock;
+        });
+        describe('when the transaction is valid', () => {
+            it('returns true', () => {
+                expect(Transaction.validTransaction(transaction)).toBe(true);
+            });
+        });
+
+        describe('when the transaction is invalid', () => {
+
+
+            describe('and a transaction outputMap value is invalid ',() => {
+                it('returns false and logs an error', () => {
+                    transaction.outputMap[senderWallet.publicKey] = 999999;
+
+                    expect(Transaction.validTransaction(transaction)).toBe(false);
+                    expect(errorMock).toHaveBeenCalled();
+                });
+            });
+
+            describe('and the transaction input signature is invalid', () => {
+                it('returns false and logs an error', () => {
+                    transaction.input.signature = new Wallet().sign('data');
+
+                    expect(Transaction.validTransaction(transaction)).toBe(false);
+                    expect(errorMock).toHaveBeenCalled();
+                });
+            });
         });
     });
 });
