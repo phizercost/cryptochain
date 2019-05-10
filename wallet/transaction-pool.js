aec301d0c0271728748595705553fdd7f9c523bf
phizercost <phizer.development@gmail.com>
2019-05-10 11:18:54 +0200

    Grabbing valid transactions

--- wallet/transaction-pool.js
+++ wallet/transaction-pool.js
@@ -1,3 +1,4 @@
+const Transaction = require('./transaction');
 class TransactionPool {
     constructor(){
         this.transactionMap = {};
@@ -14,6 +15,12 @@ class TransactionPool {
     existingTransaction({inputAddress}){
         const transactions = Object.values(this.transactionMap);
         return transactions.find(transaction => transaction.input.address === inputAddress);
+    }
+
+    validTransactions(){
+        return Object.values(this.transactionMap).filter(
+            transaction => Transaction.validTransaction(transaction)
+        );
     }
 }
 
