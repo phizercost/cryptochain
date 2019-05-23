a9f939cc2cecf8a8e1697f9cc797a0c9b6d3f478
phizercost <phizer.development@gmail.com>
2019-05-23 15:43:20 +0200

    Adding React Router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Blocks from './Blocks';
+import { Link } from 'react-router-dom';
 import logo from '../assets/logo.png';
 
 
@@ -18,14 +18,13 @@ class App extends Component {
       <img className='logo' src={logo}></img>
       <br></br>
         <div>Welcome to the blockchain...</div>
-        <br></br>
+        <br />
+        <div><Link to='/blocks'>Blocks</Link></div>
+        <br/>
         <div className='WalletInfo'>
             <div>Address: {address}</div>
             <div>Balance: {balance}</div>
         </div>
-        
-        <br />
-        <Blocks />
       </div>
     );
   }
