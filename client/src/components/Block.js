4d88540fe2e64ce855a0f5e04f3c3bef98fbf986
phizercost <phizer.development@gmail.com>
2019-05-23 13:50:30 +0200

    Toggling Transaction displays

--- client/src/components/Block.js
+++ client/src/components/Block.js
@@ -1,23 +1,65 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import { Button } from "react-bootstrap";
 
 class Block extends Component {
-    render(){
-        const {timestamp, hash, data} = this.props.block;
+  state = { displayTransaction: false };
 
-        const hashDisplay = `${hash.substring(0,15)}...`;
-        const stringifiedData = JSON.stringify(data);
+  toggleTransaction = () => {
+    this.setState({ displayTransaction: !this.state.displayTransaction });
+  };
 
-        const dataDisplay = stringifiedData.length > 35 ? `${stringifiedData.substring(0,35)}...` : stringifiedData;
+  get displayTransaction() {
+    const { data } = this.props.block;
+    const stringifiedData = JSON.stringify(data);
 
+    const dataDisplay =
+      stringifiedData.length > 35
+        ? `${stringifiedData.substring(0, 35)}...`
+        : stringifiedData;
 
-        return (
-            <div className='Block'>
-                <div>Hash: {hashDisplay}</div>
-                <div>Teimestamp: {new Date(timestamp).toLocaleString()}</div>
-                <div>Data: {dataDisplay}</div>
-            </div>
-        );
+    if (this.state.displayTransaction) {
+      return (
+        <div>
+          {JSON.stringify(data)}
+          <br />
+          <Button
+            bsStyle="danger"
+            bsSize="small"
+            onClick={this.toggleTransaction}
+          >
+            Show Less
+          </Button>
+        </div>
+      );
     }
-};
+
+    return (
+      <div>
+        <div>Data: {dataDisplay}</div>
+        <Button
+          bsStyle="danger"
+          bsSize="small"
+          onClick={this.toggleTransaction}
+        >
+          Show More
+        </Button>
+      </div>
+    );
+  }
+  render() {
+    console.log("this.displayTransactions:", this.displayTransaction);
+    const { timestamp, hash } = this.props.block;
+
+    const hashDisplay = `${hash.substring(0, 15)}...`;
+
+    return (
+      <div className="Block">
+        <div>Hash: {hashDisplay}</div>
+        <div>Timestamp: {new Date(timestamp).toLocaleString()}</div>
+        {this.displayTransaction}
+      </div>
+    );
+  }
+}
 
 export default Block;
