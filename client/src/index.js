bbf8f977fd9da93b8b04ed8993ae0e5137b52b2e
phizercost <phizer.development@gmail.com>
2019-05-23 16:43:29 +0200

    Transaction and TransactionPool Components

--- client/src/index.js
+++ client/src/index.js
@@ -5,12 +5,16 @@ import history from './history';
 import App from './components/App';
 import Blocks from './components/Blocks';
 import './index.css';
+import ConductTransaction from './components/ConductTransaction';
+import TransactionPool from './components/TransactionPool';
  
 render(
     <Router history={history}>
         <Switch>
             <Route exact={true} path='/' component = {App} />
             <Route path='/blocks' component = {Blocks} />
+            <Route path='/conduct-transaction' component = {ConductTransaction} />
+            <Route path='/transaction-pool' component = {TransactionPool} />
         </Switch>
     </Router>,
     document.getElementById('root')
