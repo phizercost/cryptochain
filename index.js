5d326a6ac75ed5064d6cd6bd31d06c3f5c68ab97
phizercost <phizer.development@gmail.com>
2019-05-23 22:20:35 +0200

    Fixing PORT issue

--- index.js
+++ index.js
@@ -145,10 +145,11 @@ if(isDevelopment){
 let PEER_PORT;
 
 if(process.env.GENERATE_PEER_PORT === 'true') {
-    PEER_PORT = DEFAULT_PORT + 1-1000; Math.ceil(Math.random() * 1000);
+    // PEER_PORT = DEFAULT_PORT + 1-1000; Math.ceil(Math.random() * 1000);
+    PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
 }
 
-const PORT = PEER_PORT || PEER_PORT || DEFAULT_PORT; 
+const PORT = process.env.PORT || PEER_PORT || DEFAULT_PORT; 
 app.listen(PORT, () => {
     console.log(`listening at localhost:${PORT}`);
 
