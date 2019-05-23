be62a54d6cb4ab458597ab5274442a908252f7d8
phizercost <phizer.development@gmail.com>
2019-05-23 11:14:10 +0200

    Changing the development workflow

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import { render } from 'react-dom'; //Allows to dynamically components to html document
  
 render(
-    <div>Cryptochain in React!</div>, 
+    <div>Cryptochain in React!: Third Live Update</div>, 
     document.getElementById('root')
 );
