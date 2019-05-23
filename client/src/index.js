fc124658cc37f5f00cc3ef41e8384992aac2b2c7
phizercost <phizer.development@gmail.com>
2019-05-23 11:02:18 +0200

    Build React in the Frontend

---
+++ client/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom'; //Allows to dynamically components to html document
+ 
+render(
+    <div>Cryptochain in React!</div>, 
+    document.getElementById('root')
+);
